c2f48210a208bd41ef571d1363d772f235184bab
Sabina <iamsabina21@gmail.com>
2022-03-11 17:33:39 +0500

    comment2

--- BankSystem/BankSystem/CurrentAccount.cs
+++ BankSystem/BankSystem/CurrentAccount.cs
@@ -8,6 +8,7 @@ namespace BankSystem
 {
     class CurrentAccount : Account
     {
+        //вап
         public new bool IsDeposit { get; set; } = false;
 
         public CurrentAccount(string Number, double Balance)
